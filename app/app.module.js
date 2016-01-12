c43b75c8741640eb7cc966132e24d800ef4454d8
Marek Pietrzak <marek.pietrzak@actech.pl>
2016-01-12 11:36:46 +0100

    .

--- app/app.module.js
+++ app/app.module.js
@@ -1,10 +1,32 @@
 (function(){
 
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Hello, world! I am a CommentBox.</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
--- build/app.module.js
+++ build/app.module.js
@@ -1,13 +1,43 @@
 (function () {
 
-  var CommentBox = React.createClass({
-    displayName: 'CommentBox',
+  var CommentList = React.createClass({
+    displayName: "CommentList",
 
     render: function () {
       return React.createElement(
-        'div',
-        { className: 'commentBox' },
-        'Hello, world! I am a CommentBox.'
+        "div",
+        { className: "commentList" },
+        "Hello, world! I am a CommentList."
+      );
+    }
+  });
+
+  var CommentForm = React.createClass({
+    displayName: "CommentForm",
+
+    render: function () {
+      return React.createElement(
+        "div",
+        { className: "commentForm" },
+        "Hello, world! I am a CommentForm."
+      );
+    }
+  });
+
+  var CommentBox = React.createClass({
+    displayName: "CommentBox",
+
+    render: function () {
+      return React.createElement(
+        "div",
+        { className: "commentBox" },
+        React.createElement(
+          "h1",
+          null,
+          "Hello, world! I am a CommentBox."
+        ),
+        React.createElement(CommentList, null),
+        React.createElement(CommentForm, null)
       );
     }
   });
