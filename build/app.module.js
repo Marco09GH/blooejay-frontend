793ee166d8142c12c0db9be079f82606d730bdce
Marek Pietrzak <marek.pietrzak@actech.pl>
2016-01-12 10:53:34 +0100

    .

---
+++ build/app.module.js
@@ -0,0 +1,20 @@
+(function () {
+	var app = angular.module('store', []);
+
+	app.controller('StoreController', function () {
+		this.products = gems;
+	});
+
+	app.controller('RestifyController', ['$http', function ($http) {
+
+		this.movies = "";
+
+		var temp = this;
+
+		$http.get('/api/get_movies').success(function (data) {
+			temp.movies = data;
+		}).error(function (e) {
+			//temp.greetings = e.message;
+		});
+	}]);
+})();
