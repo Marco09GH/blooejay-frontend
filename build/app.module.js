a3f132569abae660597e166dbff57e51e6f3ab33
Marek Pietrzak <marek.pietrzak@actech.pl>
2016-01-14 10:15:18 +0100

    .

--- build/app.module.js
+++ build/app.module.js
@@ -2,9 +2,9 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _components = require("components");
+var _photowall = require("components/photowall");
 
-var _components2 = _interopRequireDefault(_components);
+var _photowall2 = _interopRequireDefault(_photowall);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -159,7 +159,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
         return React.createElement(
           "div",
           null,
-          React.createElement(_components2.default, null),
+          React.createElement(_photowall2.default, null),
           React.createElement(
             "h1",
             { onClick: this.handleClick.bind(this) },
